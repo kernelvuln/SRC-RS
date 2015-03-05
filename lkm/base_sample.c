0f1f1930a1313fb356c04fae80c6610c598d0eb8
Oliver <rootkitsecurity@gmail.com>
2015-03-05 15:33:42 +0800

    lkm: add a base lkm of sample code

---
+++ lkm/base_sample.c
@@ -0,0 +1,20 @@
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");
+
+static int __init mod_init(void)
+{
+	printk(KERN_ALERT "[+] This is a simple example!\n");
+	return 0;
+}
+
+static void __exit mod_exit(void)
+{
+	printk(KERN_ALERT "[+] Goodbye!\n");
+}
+
+module_init(mod_init);
+module_exit(mod_exit);
+
+MODULE_AUTHOR("n/a");
+MODULE_DESCRIPTION("n/a");
