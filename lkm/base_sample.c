579a4c2d3892b7c3bf0ec845c05aefa097a37de4
Oliver <rootkitsecurity@gmail.com>
2015-03-06 10:58:01 +0800

    lkm: update a base lkm of sample code

--- lkm/base_sample.c
+++ lkm/base_sample.c
@@ -1,6 +1,11 @@
+/*
+ *  sample code
+ */
 #include <linux/module.h>
 
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("n/a");
+MODULE_DESCRIPTION("n/a");
 
 static int __init mod_init(void)
 {
@@ -15,6 +20,3 @@ static void __exit mod_exit(void)
 
 module_init(mod_init);
 module_exit(mod_exit);
-
-MODULE_AUTHOR("n/a");
-MODULE_DESCRIPTION("n/a");
